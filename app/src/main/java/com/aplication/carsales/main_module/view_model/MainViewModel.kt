dfca6830836b3682c07a644f50f7f36c350e916b
Fabian Guerrero <fabian.guerrem@gmail.com>
2022-10-18 13:12:12 -0300

    unnecessary imports deleted + formattted code

--- app/src/main/java/com/aplication/carsales/main_module/view_model/MainViewModel.kt
+++ app/src/main/java/com/aplication/carsales/main_module/view_model/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.aplication.carsales.main_module.view_model
 
-import android.util.Log
 import androidx.lifecycle.*
 import com.aplication.carsales.R
 import com.aplication.carsales.common.entities.CovidDataEntity
@@ -8,7 +7,7 @@ import com.aplication.carsales.main_module.model.MainRepository
 import kotlinx.coroutines.launch
 import java.net.UnknownHostException
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
     private val repository = MainRepository()
 
     private val result = MutableLiveData<CovidDataEntity>()
@@ -23,19 +22,18 @@ class MainViewModel: ViewModel() {
     private val loading = MutableLiveData<Boolean>()
     fun isLoaded() = loading
 
-    suspend fun getCovidDataFromDate(date: String){
+    suspend fun getCovidDataFromDate(date: String) {
         viewModelScope.launch {
             try {
                 loading.value = false
-                val  resultServer = repository.getCovidDataFromDate(date)
+                val resultServer = repository.getCovidDataFromDate(date)
                 result.value = resultServer
                 dateSelected.value = date.split("-").reversed().joinToString("-")
-            } catch (e: UnknownHostException){
+            } catch (e: UnknownHostException) {
                 snackBarMsg.value = R.string.unknown_host_error
-            }catch (e: Exception) {
+            } catch (e: Exception) {
                 snackBarMsg.value = R.string.main_error
-            }
-            finally {
+            } finally {
                 loading.value = true
             }
         }
